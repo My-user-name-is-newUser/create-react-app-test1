eb999f7dfb9f53bb32c245540fe6383eea2b442e
Pavel <you@example.com>
2025-09-03 00:42:00 +0300

    текущий год под ссылкой Learn React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
+export const App = () => {
+	const currentYear = new Date().getFullYear();
   return (
     <div className="App">
       <header className="App-header">
@@ -17,9 +17,8 @@ function App() {
         >
           Learn React
         </a>
+				<div>{currentYear}</div>
       </header>
     </div>
   );
 }
-
-export default App;
