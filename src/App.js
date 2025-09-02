0bf48f3baece6dbb14dc0a23b6adc37b93750ea1
Pavel <you@example.com>
2025-09-03 01:13:06 +0300

    код без JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
+import { createElement } from 'react';
 
 export const App = () => {
 	const currentYear = new Date().getFullYear();
   return (
-    <div className="App">
+		/*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +20,33 @@ export const App = () => {
         </a>
 				<div>{currentYear}</div>
       </header>
-    </div>
-  );
+    </div>*/
+		createElement(
+			'div',
+			{ className: 'App' },
+			createElement(
+				'header',
+				{ className: 'App-header' },
+				createElement('img', { className: 'App-logo', src: logo }),
+				createElement(
+					'p',
+					null,
+					'Edit',
+					createElement('code', null, 'src/App.js'),
+					' and save to reload.',
+				),
+				createElement(
+					'a',
+					{
+						className: 'App-link',
+						href: 'https://reactjs.org',
+						target: '_blank',
+						rel: 'noopener noreferrer',
+					},
+					'Learn React',
+				),
+				createElement('div', null, currentYear),
+			),
+		)
+	);
 }
